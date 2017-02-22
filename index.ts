2e53418b393be1628db022118da44050d40ddcb2
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-22 14:15:32 -0500

    Add shared module

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+})
+export class SharedModule {
+  constructor() {
+    console.log('Hello world!', 'SharedModule()');
+  }
+}
+
 export function testingStuff() {
-  console.log('Hello from', 'testingStuff()');
+  console.log('Hello world!', 'testingStuff()');
 }
